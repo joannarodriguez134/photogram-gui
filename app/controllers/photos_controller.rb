13ffc9a446c06fd2064ed7b06cc776195fbf4cbe
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-14 21:09:58 +0000

    Added delete photo functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,5 +6,29 @@ class PhotosController < ApplicationController
     render(template: "photos_templates/index")
   end
 
+  def show
+
+    photo_id_url = params.fetch("path_photo_id")
+
+    photo_record = Photo.where(:id => photo_id_url)
+
+    @photo = photo_record.at(0)
+
+    render(template: "photos_templates/show")
+  end
+
+  def destroy
+
+    photo_id_url = params.fetch("path_photo_id")
+
+    photo_record = Photo.where(:id => photo_id_url)
+
+    photo = photo_record.at(0)
+
+    photo.destroy
+    
+    redirect_to("/photos")
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
 
   get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:path_photo_id", { :controller => "photos", :action => "destroy"})
+
 
 end
